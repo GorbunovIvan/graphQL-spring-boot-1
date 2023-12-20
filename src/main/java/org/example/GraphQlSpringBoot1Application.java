919acd4d169f30074bd297077e183a62f259919b
GorbunovIvan <gorbunovvivan@gmail.com>
2023-12-20 09:30:28 +0200

    init

---
+++ src/main/java/org/example/GraphQlSpringBoot1Application.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphQlSpringBoot1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphQlSpringBoot1Application.class, args);
+    }
+
+}
